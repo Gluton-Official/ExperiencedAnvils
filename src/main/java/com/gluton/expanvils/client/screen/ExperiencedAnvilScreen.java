ad1779c68979adc31c351f802d6d290381aa0354
Gluton <glutonofficial@gmail.com>
2021-02-06 16:09:39 -0500

    Absent server side compatability will result in normal anvil behavior

--- src/main/java/com/gluton/expanvils/client/screen/ExperiencedAnvilScreen.java
+++ src/main/java/com/gluton/expanvils/client/screen/ExperiencedAnvilScreen.java
@@ -8,11 +8,16 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.container.RepairContainer;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
 /**
  * @author Gluton
  */
+@OnlyIn(Dist.CLIENT)
 public class ExperiencedAnvilScreen extends AnvilScreen {
+
+	private static final ITextComponent TOO_EXPENSIVE = new TranslationTextComponent("container.repair.expensive");
 
 	public ExperiencedAnvilScreen(RepairContainer container, PlayerInventory playerInventory, ITextComponent title) {
 		super(container, playerInventory, title);
@@ -21,26 +26,30 @@ public class ExperiencedAnvilScreen extends AnvilScreen {
 	@Override
 	protected void drawGuiContainerForegroundLayer(MatrixStack matrixStack, int x, int y) {
 		RenderSystem.disableBlend();
-		this.font.func_243248_b(matrixStack, this.title, (float)this.titleX, (float)this.titleY, 4210752);
-		this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(), (float)this.playerInventoryTitleX, (float)this.playerInventoryTitleY, 4210752);
-	    int i = this.container.getMaximumCost();
-	    if (i > 0) {
-	    	int j = 8453920;
-	        ITextComponent itextcomponent;
-	        if (!this.container.getSlot(2).getHasStack()) {
-	        	itextcomponent = null;
-	        } else {
-	        	itextcomponent = new TranslationTextComponent("container.repair.cost", i);
-	        	if (!this.container.getSlot(2).canTakeStack(this.playerInventory.player)) {
-	        		j = 16736352;
-	        	}
-	        }
+		this.font.func_243248_b(matrixStack, this.title, (float) this.titleX, (float) this.titleY, 4210752);
+		this.font.func_243248_b(matrixStack, this.playerInventory.getDisplayName(), (float) this.playerInventoryTitleX, (float) this.playerInventoryTitleY, 4210752);
+		int xpCost = this.container.getMaximumCost();
+		if (xpCost > 0) {
+			int textColor = 8453920;
+			ITextComponent textComponent;
+			// if the level limit has not been removed server side and the item is over 39, no itemstack will be in the ouput, so the item will be too expensive
+			if (xpCost >= 40 && !this.minecraft.player.abilities.isCreativeMode && !this.container.getSlot(2).getHasStack() && this.container.getSlot(1).getHasStack()) {
+				textComponent = TOO_EXPENSIVE;
+				textColor = 16736352;
+			} else if (!this.container.getSlot(2).getHasStack()) {
+				textComponent = null;
+			} else {
+				textComponent = new TranslationTextComponent("container.repair.cost", xpCost);
+				if (!this.container.getSlot(2).canTakeStack(this.playerInventory.player)) {
+					textColor = 16736352;
+				}
+			}
 
-	        if (itextcomponent != null) {
-	        	int k = this.xSize - 8 - this.font.getStringPropertyWidth(itextcomponent) - 2;
-	        	fill(matrixStack, k - 2, 67, this.xSize - 8, 79, 1325400064);
-	        	this.font.func_243246_a(matrixStack, itextcomponent, (float)k, 69.0F, j);
-	        }
-	    }
+			if (textComponent != null) {
+				int xPosition = this.xSize - 8 - this.font.getStringPropertyWidth(textComponent) - 2;
+				fill(matrixStack, xPosition - 2, 67, this.xSize - 8, 79, 1325400064);
+				this.font.func_243246_a(matrixStack, textComponent, (float) xPosition, 69.0F, textColor);
+			}
+		}
 	}
 }
